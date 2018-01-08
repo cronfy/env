05ca7ac403c92539c02221711a52122b0852fb1b
Cronfy <cronfy@gmail.com>
2018-01-08 10:06:30 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# env
-PHP environment
+# PHP environment
+
+Provides framework agnostic access to environment data, that can be loaded as array.
+
+Example:
+
+```php
+// any environment loader can be used, we just need $anvironment to be an array
+$environment = (new josegonzalez\Dotenv\Loader(__DIR__ . '/.env'))
+    ->parse()
+    ->toArray();
+
+// load envirinment data
+Env::load($environment);
+
+// use environment variables
+echo Env::get('MYSQL_USER');
+
+// set variables
+Env::set('MYSQL_USER', 'value');
+```
---
+++ src/Env.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace cronfy\env;
+
+class Env {
+
+    protected static $debug;
+
+    /**
+     * Helper function to get/set debug state
+     * @param bool $value
+     * @return bool
+     */
+    public static function isDebug($value = null) {
+        if (!$value !== null) {
+            static::$debug = (bool) $value;
+        }
+
+        if (!is_null(static::$debug)) {
+            return static::$debug;
+        }
+
+        return false;
+    }
+
+    protected static $vendorDir;
+
+    /**
+     * Helper function to get/set vendor directory
+     * @param string $value
+     * @return bool
+     */
+    public static function getVendorDir($value) {
+        if (!$value !== null) {
+            static::$vendorDir = $value;
+        }
+
+        return static::$vendorDir ?: dirname(dirname(dirname(__DIR__)));
+    }
+
+    static protected $_env;
+
+    /**
+     * Load environment data
+     * @param $data
+     */
+    public static function load($data) {
+        static::$_env = $data;
+    }
+
+    /**
+     * Get environment variable
+     *
+     * @param $name
+     * @param null $default
+     * @return null
+     * @throws \Exception
+     */
+    public static function get($name, $default = null) {
+        if (!isset(static::$_env[$name])) {
+            if (null === $default) {
+                throw new \Exception("Environment variable $name is not set");
+            } else {
+                return $default;
+            }
+        }
+        return static::$_env[$name];
+    }
+
+    /**
+     * Set environment variable
+     *
+     * @param $name
+     * @param mixed $value
+     * @return mixed
+     * @throws \Exception
+     */
+    public static function set($name, $value) {
+        static::$_env[$name] = $value;
+        return $value;
+    }
+
+}
